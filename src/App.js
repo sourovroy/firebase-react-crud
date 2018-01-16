c387b8cc6f8d9c4a7350b5a2649298a62ada7cd0
Soruov Roy <soruovfci@gmail.com>
2018-01-16 19:34:15 +0600

    read data from firebase

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
-import firebase from "firebase";
+import { usersRef, manageUserItems } from "./database";
 
 class App extends Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			text: '',
-			value: 'Something'
+			items: []
 		};
 	}
 
 	componentDidMount(){
-		var config = {
-			apiKey: "AIzaSyDzLpbTPixl4q57m5E1as9CvUCS-0dYAqA",
-			authDomain: "sourov-first-project.firebaseapp.com",
-			databaseURL: "https://sourov-first-project.firebaseio.com",
-			projectId: "sourov-first-project",
-			storageBucket: "sourov-first-project.appspot.com",
-			messagingSenderId: "585255870499"
-		};
-		firebase.initializeApp(config);
+		usersRef.once('value', snap => this.manageItemsList(snap) );
+		usersRef.on('value', snap => this.manageItemsList(snap) );
+			
 
-		var dbRef = firebase.database().ref().child('text');
-		dbRef.on('value', snap => this.setState({text: snap.val()}));
+		// usersRef.push({
+		// 	name: "Sourov Roy"
+		// });
 	}
 
-	render() {
+	manageItemsList(snapshot){
+		this.setState({ 
+			items: manageUserItems(snapshot) 
+		});
+	}
+
+	render(){
+		var usersList = this.state.items.map((item, index) => 
+				<li key={index}>{item.name}</li>
+			);
+			
 		return (
 			<div className="App">
-				<h1>{this.state.text} and {this.state.value}</h1>
+				<ul>
+					{usersList}
+				</ul>
 			</div>
 		);
 	}
---
+++ src/database.js
@@ -0,0 +1,28 @@
+import firebase from "firebase";
+
+firebase.initializeApp({
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID
+});
+
+export const usersRef = firebase.database().ref('users');
+
+/**
+ * Users item map with react array
+ * @return Array of firebase object
+ */
+export function manageUserItems(snapshot){
+	var items = snapshot.val(),
+		returnItems = [];
+	for(let item in items){
+		returnItems.push({
+			'id': item,
+			'name': items[item].name
+		});
+	}
+	return returnItems;
+}
